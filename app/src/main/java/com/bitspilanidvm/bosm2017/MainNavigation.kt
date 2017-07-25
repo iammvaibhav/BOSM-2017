4a0030f9e1c221d3738d7dda210daa409b7f38d3
Vaibhav Maheshwari <v4ibh4v.m@gmail.com>
2017-07-25 18:14:58 +0530

    Initial Preparation of Main Navigation Activity

--- app/src/main/java/com/bitspilanidvm/bosm2017/MainNavigation.kt
+++ app/src/main/java/com/bitspilanidvm/bosm2017/MainNavigation.kt
@@ -2,11 +2,19 @@ package com.bitspilanidvm.bosm2017
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 
 class MainNavigation : AppCompatActivity() {
+
+    lateinit var toolBar: Toolbar
+    lateinit var mainNavRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_navigation)
+
+        toolBar = findViewById(R.id.toolBar)
+        mainNavRecyclerView = findViewById(R.id.mainNavRecyclerView)
     }
 }
