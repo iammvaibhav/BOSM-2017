7cf1054ff48d5c8a4d320a69455932d4ca340b61
Vaibhav Maheshwari <v4ibh4v.m@gmail.com>
2017-07-25 17:18:47 +0530

    Initialized Android Project For BOSM 2017

---
+++ app/src/main/java/com/bitspilanidvm/bosm2017/MainNavigation.kt
@@ -0,0 +1,12 @@
+package com.bitspilanidvm.bosm2017
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainNavigation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_navigation)
+    }
+}
