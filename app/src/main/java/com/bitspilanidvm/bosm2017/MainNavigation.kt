00d9f5eef2a256e68fe85ef469901c56225fa117
Vaibhav Maheshwari <v4ibh4v.m@gmail.com>
2017-08-04 01:14:28 +0530

    Add Recycler View Animations

--- app/src/main/java/com/bitspilanidvm/bosm2017/MainNavigation.kt
+++ app/src/main/java/com/bitspilanidvm/bosm2017/MainNavigation.kt
@@ -5,36 +5,65 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.util.DisplayMetrics
+import android.view.View
 
 class MainNavigation : AppCompatActivity() {
 
     lateinit var toolBar: Toolbar
     lateinit var mainNavRecyclerView: RecyclerView
 
-    var dataItems = ArrayList<Pair<String, Int>>()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_navigation)
 
-        window.setBackgroundDrawableResource(R.drawable.temp)
-
-        dataItems.add(Pair("This is A", android.R.color.holo_blue_dark))
-        dataItems.add(Pair("This is B", android.R.color.holo_red_dark))
-        dataItems.add(Pair("This is C", android.R.color.holo_orange_dark))
-        dataItems.add(Pair("This is D", android.R.color.holo_green_dark))
-        dataItems.add(Pair("This is E", android.R.color.holo_purple))
-        dataItems.add(Pair("This is F", android.R.color.black))
-        dataItems.add(Pair("This is G", android.R.color.holo_green_light))
-        dataItems.add(Pair("This is H", android.R.color.holo_blue_bright))
-        dataItems.add(Pair("This is I", android.R.color.holo_red_light))
-
-        toolBar = findViewById(R.id.toolBar)
         mainNavRecyclerView = findViewById(R.id.mainNavRecyclerView)
+        toolBar = findViewById(R.id.toolBar)
 
-        mainNavRecyclerView.layoutManager = LinearLayoutManager(this)
-        mainNavRecyclerView.adapter = MainRecyclerViewAdapter(dataItems)
+        window.setBackgroundDrawableResource(R.drawable.r)
+        setSupportActionBar(toolBar)
+        supportActionBar?.title = ""
+
+        val layoutManager = LinearLayoutManager(this)
+        val recyclerAdapter = MainRecyclerViewAdapter(getMainNavData())
+        val recyclerAnimatorAdapter = CustomAnimator(recyclerAdapter)
+
+        with(mainNavRecyclerView){
+            setLayoutManager(layoutManager)
+            adapter = recyclerAnimatorAdapter
+            setHasFixedSize(true)
+            setItemViewCacheSize(20)
+            isDrawingCacheEnabled = true
+            drawingCacheQuality = View.DRAWING_CACHE_QUALITY_HIGH
+        }
 
     }
+
+    fun Int.toPx() : Int{
+        var displayMetrics = DisplayMetrics()
+        this@MainNavigation.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return (this * displayMetrics.density).toInt()
+    }
+
+    fun Int.toDp() : Int{
+        var displayMetrics = DisplayMetrics()
+        this@MainNavigation.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return (this / displayMetrics.density).toInt()
+    }
+}
+
+fun getMainNavData() : ArrayList<Pair<String, Int>>{
+
+    var dataItems = ArrayList<Pair<String, Int>>()
+
+    dataItems.add(Pair("HIGHLIGHTS", R.drawable.w))
+    dataItems.add(Pair("SCIENCE", R.drawable.q))
+    dataItems.add(Pair("GAMING", R.drawable.u))
+    dataItems.add(Pair("MOVIES", R.drawable.w))
+    dataItems.add(Pair("HIGHLIGHTS", R.drawable.q))
+    dataItems.add(Pair("SCIENCE", R.drawable.u))
+    dataItems.add(Pair("GAMING", R.drawable.w))
+    dataItems.add(Pair("MOVIES", R.drawable.q))
+
+    return dataItems
 }
--- app/src/main/java/com/bitspilanidvm/bosm2017/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/bitspilanidvm/bosm2017/MainRecyclerViewAdapter.kt
@@ -4,22 +4,23 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
-class MainRecyclerViewAdapter(dataList: List<Pair<String, Int>>) : RecyclerView.Adapter<MainItemViewHolder>(){
-    var dataList : List<Pair<String, Int>>
+class MainRecyclerViewAdapter(var dataList: List<Pair<String, Int>>) : RecyclerView.Adapter<MainItemViewHolder>() {
 
-    init {
-        this.dataList = dataList
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MainItemViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_main_nav_recycler_view,
-                                                                parent, false)
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_main_nav_recycler_view, parent, false)
         return MainItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MainItemViewHolder?, position: Int) {
         holder?.itemText?.text = dataList[position].first
-        holder?.itemImage?.setBackgroundResource(dataList[position].second)
+        holder?.itemImage?.setImageResource(dataList[position].second)
+
+    }
+
+    override fun onViewAttachedToWindow(holder: MainItemViewHolder?) {
+        super.onViewAttachedToWindow(holder)
+
     }
 
     override fun getItemCount() = dataList.size
