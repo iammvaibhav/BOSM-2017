a6f4525def470309d311136e0771640e9da57abd
Vaibhav Maheshwari <v4ibh4v.m@gmail.com>
2017-08-19 03:12:00 +0530

    Modified Main Recycler View Adapter

--- app/src/main/java/com/bitspilanidvm/bosm2017/Adapter_MainRecyclerView.kt
+++ app/src/main/java/com/bitspilanidvm/bosm2017/Adapter_MainRecyclerView.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 
 class Adapter_MainRecyclerView(var dataList: List<Pair<String, Int>>, val listener: MainRecyclerViewClickListener) : RecyclerView.Adapter<ViewHolder_MainItem>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder_MainItem {
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_main_nav_recycler_view, parent, false)
         return ViewHolder_MainItem(view)
