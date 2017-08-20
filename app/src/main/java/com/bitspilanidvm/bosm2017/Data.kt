fae6932077bdf2f44ea1f6a28c8c9087892e6a98
Vaibhav Maheshwari <v4ibh4v.m@gmail.com>
2017-08-20 05:41:05 +0530

    Provided global data

--- app/src/main/java/com/bitspilanidvm/bosm2017/Data.kt
+++ app/src/main/java/com/bitspilanidvm/bosm2017/Data.kt
@@ -1,7 +1,8 @@
 package com.bitspilanidvm.bosm2017
 
 class Data{
-    companion object STATIC_DATA{
-
+    companion object GLOBAL_DATA{
+        var textSize = 40f
+        var currSelectedPage = 0
     }
 }
